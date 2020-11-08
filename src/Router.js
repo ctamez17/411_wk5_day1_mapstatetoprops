454ac309dec58aed889f8196642b98dda6ca02c9
Cody Tamez <codytamez17@gmail.com>
2020-11-08 10:33:16 -0600

    changes

--- src/Router.js
+++ src/Router.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import { Switch, Route } from 'react-router'
-import Home from './components/Home'
-import About from './components/About'
-import Car from './components/Car'
-import Dashboard from './components/Dashboard'
+import React from "react";
+import { Switch, Route } from "react-router";
+import Home from "./Containers/Home";
+import About from "./components/About";
+import Car from "./Containers/Car";
+import Dashboard from "./Containers/Dashboard";
 
 const Router = () => {
-    return (
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/car/:id" component={Car} />
-            <Route path="/dashboard" component={Dashboard} />
-        </Switch>
-    );
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/car/:id" component={Car} />
+      <Route path="/dashboard" component={Dashboard} />
+    </Switch>
+  );
 };
 
 export default Router;
